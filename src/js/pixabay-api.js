967796a2904460130a1d52b428305b2761a013d2
alena_sinitsa <alenasinitsa85@gmail.com>
2024-07-31 21:10:52 +0300

    pixabay axios

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,13 +1,21 @@
+import axios from 'axios';
+
 const API_KEY = '45098988-0aca0e44808ea00320f5f0e3c';
-const BASE_URL = 'https://pixabay.com/api/';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 
 export async function fetchImages(query) {
-  const response = await fetch(
-    `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
-  );
-  if (!response.ok) {
+  try {
+    const response = await axios.get('', {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    });
+    return response.data.hits;
+  } catch (error) {
     throw new Error('Failed to fetch images');
   }
-  const data = await response.json();
-  return data.hits;
 }
