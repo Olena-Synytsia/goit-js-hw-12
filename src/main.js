3083009dffe3a1d5398f59695b0271fef3881331
alena_sinitsa <alenasinitsa85@gmail.com>
2024-08-02 23:16:39 +0300

    4

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ const loadMoreBtn = document.querySelector('[data-action="load-more"]');
 let lightbox;
 let currentPage = 1;
 let totalHits = 0;
+let query = '';
 
 class ButtonService {
   constructor(buttonEl, hiddenClass) {
@@ -35,7 +36,7 @@ const buttonService = new ButtonService(loadMoreBtn, 'is-hidden');
 
 form.addEventListener('submit', async event => {
   event.preventDefault();
-  const query = event.target.elements['query'].value.trim();
+  query = event.target.elements['query'].value.trim();
   currentPage = 1;
 
   if (!query) {
@@ -54,6 +55,7 @@ form.addEventListener('submit', async event => {
         'Sorry, there are no images matching your search query. Please try again!'
       );
       gallery.innerHTML = '';
+      buttonService.hide();
     } else {
       gallery.innerHTML = createGalleryMarkup(images);
       lightbox?.destroy();
@@ -63,6 +65,10 @@ form.addEventListener('submit', async event => {
       });
       totalHits = images.length;
       buttonService.show();
+      if (totalHits < 15) {
+        buttonService.hide();
+        showError("We're sorry, but you've reached the end of search results.");
+      }
     }
   } catch (error) {
     showError('Failed to fetch images');
@@ -72,5 +78,41 @@ form.addEventListener('submit', async event => {
   }
 });
 
-console.log(buttonService);
-console.dir(loadMoreBtn);
+loadMoreBtn.addEventListener('click', async () => {
+  currentPage += 1;
+  loader.style.display = 'flex';
+
+  try {
+    const images = await fetchImages(query, currentPage);
+
+    if (images.length === 0) {
+      buttonService.hide();
+      showError('На жаль, ви досягли кінця результатів пошуку.');
+    } else {
+      gallery.insertAdjacentHTML('beforeend', createGalleryMarkup(images));
+      lightbox?.destroy();
+      lightbox = new SimpleLightbox('.gallery-link', {
+        captionsData: 'alt',
+        captionDelay: 250,
+      });
+
+      const { height: cardHeight } = document
+        .querySelector('.gallery-image')
+        .getBoundingClientRect();
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+      });
+
+      totalHits += images.length;
+      if (totalHits >= 15 * currentPage) {
+        buttonService.hide();
+        showError('На жаль, ви досягли кінця результатів пошуку.');
+      }
+    }
+  } catch (error) {
+    showError('Не вдалося завантажити зображення');
+  } finally {
+    loader.style.display = 'none';
+  }
+});
